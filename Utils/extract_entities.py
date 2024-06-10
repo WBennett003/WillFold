0c74a57f248dc95fd9c7ceb3a7f3b08f4fbfb14e
tyrtamus <89939582+tyrtamus@users.noreply.github.com>
2024-06-10 23:58:04 +0100

    Add files via upload
    Added function to get atom coords

--- Utils/extract_entities.py
+++ Utils/extract_entities.py
@@ -1,4 +1,10 @@
 from Bio.PDB.MMCIF2Dict import MMCIF2Dict
+import json
+
+
+with open('token_ref.json') as rf:
+    residue_names = json.load(rf)
+
 
 # Parse MMCIF with biopython
 
@@ -18,7 +24,7 @@ def parse_nonpolymer(struct_dict, entity_id : int):
     entity_index = struct_dict['_pdbx_entity_nonpoly.entity_id'].index(str(entity_id))
     return [struct_dict['_pdbx_entity_nonpoly.comp_id'][entity_index]]
 
-
+# TODO: trim residues with no data
 def parse_CIF(filepath : str):
 
     struct_dict = MMCIF2Dict(filepath)
@@ -41,10 +47,45 @@ def parse_CIF(filepath : str):
 
     return structure
 
-def test():
+
+def extract_coordinates(filepath : str, N_max_atom : int):
+    
+    struct_dict = MMCIF2Dict(filepath)
+
+    coordinates = []
+    res = []
+
+    curr = struct_dict['_atom_site.label_seq_id'][0]
+
+    for i in range(0, len(struct_dict['_atom_site.id'])):
+
+        row = {
+            'label_seq_id' : struct_dict['_atom_site.label_seq_id'][i],
+            'label_comp_id' : struct_dict['_atom_site.label_comp_id'][i], 
+            'cartn_x' : float(struct_dict['_atom_site.Cartn_x'][i]), 
+            'cartn_y' : float(struct_dict['_atom_site.Cartn_y'][i]), 
+            'cartn_z' : float(struct_dict['_atom_site.Cartn_z'][i])
+        }
+
+        if row['label_seq_id'] != curr or row['label_comp_id'] not in residue_names : 
+            # pad res
+            coordinates.append(res) # + ([0] * (N_max_atom - len(res)))
+            res = []
+            curr = row['label_seq_id']
+
+        res.append([row['cartn_x'], row['cartn_y'], row['cartn_z']])
+
+    coordinates.append(res + ([0] * (N_max_atom - len(res))))
+
+    print(int(struct_dict['_atom_site.id'][-1]))
+
+    return coordinates
+
+
+def test_generic(function, args=None):
 
     import os
-    
+
     def print_data(data):
         for d in data:
             print(d)
@@ -52,4 +93,44 @@ def test():
 
     for file in os.listdir('data'):
         print('data/' + file)
-        print_data(parse_CIF('data/' + file))
+        if args:
+            with open('test_' + file, 'w') as wf:
+                wf.write("\n".join(str(d) for d in function('data/' + file, *args)))
+        else:
+            print_data(function('data/' + file))
+
+def plt_test():
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import os
+
+
+    def plot(filepath):
+        coords = extract_coordinates(filepath, 24)
+
+        tkn_cnt = 0
+
+        to_plt = []
+
+        for tkn in coords:
+            tkn_cnt += 1
+            for pos in tkn:
+                if pos != 0:
+                    to_plt.append(pos)
+
+        to_plt = np.array(to_plt)
+
+        print(to_plt)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')        
+        ax.scatter(to_plt[:, 0], to_plt[:, 1], to_plt[:, 2])
+        plt.show()
+
+    for file in os.listdir('data'):
+        plot('data/' + file)
+
+
+# test_generic(extract_coordinates, [24])
+plt_test()
